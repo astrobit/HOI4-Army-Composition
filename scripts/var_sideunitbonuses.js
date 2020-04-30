80a5578de1cc432fa23132f971d4cccb27be3b1a
Brian W. Mulligan <astrobit2@yahoo.com>
2020-04-29 20:29:20 -0500

    Update var_sideunitbonuses.js
    make sure that bonuses are real numbers

--- scripts/var_sideunitbonuses.js
+++ scripts/var_sideunitbonuses.js
@@ -5,14 +5,19 @@ for (j = 0; j < 2; j++)
 	for (i = 0; i < units.length; i++)
 	{
 		lclside.push(new unitclass(units[i].name,null,
-			1, // width
-			1, // organization
-			1, // HP
-			1,// soft
-			1, //hard
-			1, // defense
-			1, // breakthrough
-			1,1,1,1,1,1));
+			1.0, // width
+			1.0, // organization
+			1.0, // HP
+			1.0,// soft
+			1.0, //hard
+			1.0, // defense
+			1.0, // breakthrough
+            1.0, // armor
+            1.0, // piercing
+            1.0, // hardness
+            1.0, // speed
+            1.0, // production cost
+            1.0)); // reliability
 	}
 	sideunitbonus.push(lclside);
 }
